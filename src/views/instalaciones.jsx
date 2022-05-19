cf048e732316fb6997fc95b4dd21081f337cf262
Develop_ruiz <ivanrur95@hotmail.com>
2022-05-19 09:34:11 -0500

    se hace comiit

---
+++ src/views/instalaciones.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Instalaciones = () => {
+return (
+<>
+    <section className="pt-5">
+        <div className="row">
+            <div className="col-lg-12 text-center pt-5">
+                    <h1>INSTALACIONES</h1>
+            </div>
+            <div className="col-lg-12 text-center">
+                    <h3>ESTUDIO ELENA</h3>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        
+    </section>
+</>
+)
+}
+
+export default Instalaciones
